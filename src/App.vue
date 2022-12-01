ae935955d71db85cdf527b3c5033a7286e0608bb
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2022-12-01 15:23:07 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Football />
+</template>
+
+<script>
+import Football from "./components/Football.vue";
+export default {
+  name: "products",
+  components: {
+    Football,
+  },
+};
+</script>
---
+++ src/components/Football.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header class="border-b border-gray-500 pb-2">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="relative">
+          <div>
+            <div class="items-center pt-2">
+              <NuxtLink to="/">
+                <a
+                  href="#"
+                  class="-m-2 block p-2 font-medium text-center text-gray-900"
+                  >Football Club</a
+                >
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+  <div></div>
+</template>
+
+<script setup >
+</script>
